dab9ccf6e89537458f6c80040b6c5ffb166e590e
Logan Hennessy <loganjhennessy@gmail.com>
2026-07-21 13:17:38 -0700

    Remove hard coded region from teardown script

--- scripts/teardown.sh
+++ scripts/teardown.sh
@@ -9,7 +9,7 @@ export CLOUDSDK_CORE_VERBOSITY=error
 echo "Deleting smart-bookmarks-service from Google Cloud Run..."
 
 gcloud run services delete smart-bookmarks-service \
-  --region us-west1 \
+  --region "$REGION" \
   --quiet
 
 echo "Teardown complete! Service removed."
